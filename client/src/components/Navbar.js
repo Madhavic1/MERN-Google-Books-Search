5338298018516c3d58ca897fb0b7310cfcd3ad72
Ganesh Chittella <madhavi.0108@gmail.com>
2020-05-03 11:38:03 -0700

    Added code to make the Navbar responsive for small screens my adding a button with bootstrap class navbar-collapse

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,8 +6,11 @@ function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="/">Google Books</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsenavbar" aria-controls="collapsenavbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                <div className="collapse navbar-collapse" id="">
+                <div className="collapse navbar-collapse" id="collapsenavbar">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item ">
                             <Link to="/search"
