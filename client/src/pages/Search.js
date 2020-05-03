55956f06d7e8e55f7c3a8feb7e19245ac3b556aa
Ganesh Chittella <madhavi.0108@gmail.com>
2020-05-03 11:35:34 -0700

    Replace the Search page code with SearchHeader,SearchForm and SearchResults components

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Container from "../components/Container"
+import Col from "../components/Col"
+import Row from "../components/Row"
+import SearchForm from '../components/SearchForm'
+import SearchHeader from '../components/SearchHeader'
+import SearchResults from '../components/SearchResults'
+
 
 function Search() {
     return (
-        <div>
-            Search Component
-        </div>
+        <Container fluid="fluid">
+           <SearchHeader />
+            <SearchForm />
+            <SearchResults />
+        </Container>
     )
 }
 
