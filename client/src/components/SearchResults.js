e5a44d5d60700ac5f1bd2044c3aed2f5c6c21362
Ganesh Chittella <madhavi.0108@gmail.com>
2020-05-05 07:31:02 -0700

    imported SearchedBook.js component into SearchResults component and sent some props to render each searched page

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import SearchedBook from './SearchedBook';
 
-const descStyle={
-    font_size:{
-        fontSize:"smaller"
+const descStyle = {
+    font_size: {
+        fontSize: "smaller"
     }
 }
 
@@ -12,24 +13,15 @@ function SearchResults({ searchResults }) {
         <div className="border mt-2">
             <strong >Results </strong>
             <div className="border-bottom-0 p-2">
-            {searchResults.map(book => (
-                <div key={book.id} className="mb-1 border-bottom">
-                    <div className="d-flex">
-                        <div>
-                        <p>Title: {book.volumeInfo.title}</p>
-                        <p>Written By: {book.volumeInfo.authors}</p>
-                        </div>
-                        <div className="ml-auto">
-                            <a href={book.volumeInfo.previewLink}>View</a>
-                            <a>Save</a>
-                        </div>
-                    </div>
-                    <div className="d-flex">
-                         <img src={book.volumeInfo.imageLinks.thumbnail} className="mr-3" />
-                         <p className="ml-auto" style={descStyle.font_size}>{book.volumeInfo.description}</p>
-                    </div>
-                </div>
-            ))}
+                {searchResults.map(book => (
+                    <SearchedBook key={book.id}
+                        title={book.volumeInfo.title}
+                        author={book.volumeInfo.authors}
+                        previewLink={book.volumeInfo.previewLink}
+                        image={book.volumeInfo.imageLinks.thumbnail}
+                        description={book.volumeInfo.description} 
+                        descStyle={descStyle}/>
+                ))}
             </div>
         </div>
     )
